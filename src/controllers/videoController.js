a0d93942b342993b778d431e970763bcd2e42201
Yoonseongwon <missingtttt@gmail.com>
2022-01-07 20:24:51 +0900

    UrlparamAndPugAndRegularExpression

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,3 +1,19 @@
-export const trending = (req, res) => res.send("Home page Videos");
-export const watch = (req, res) => res.send("Watch");
-export const edit = (req, res) => res.send("Edit");
+
+export const trending = (req, res) => {
+    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    return res.render("home", {pageTitle : "Home", videos : videos});
+}
+export const see = (req, res) => {
+    console.log(req.params);
+    return res.render("watch");
+}
+export const edit = (req, res) => {
+    console.log(req.params);
+    return res.render("edit");
+}
+export const search = (req, res) => res.send("serach");
+export const upload = (req, res) => res.send("Upload Video");
+export const deleteVideo = (req, res) => {
+    console.log(req.params);
+    return res.send("Delete Video");
+}
--- src/routers/userRouter.js
+++ src/routers/userRouter.js
@@ -1,12 +1,14 @@
 import express from "express";
-import {edit} from "../controllers/userController";
-import {deleteUser} from "../controllers/userController";
+import { use } from "express/lib/application";
+import {edit, deleteUser, see, logout} from "../controllers/userController";
+
 
 const userRouter = express.Router();
 
 
-
+userRouter.get("/logout", logout);
 userRouter.get("/edit", edit);
 userRouter.get("/delete", deleteUser);
+userRouter.get(":id",see);
 
 export default userRouter;
