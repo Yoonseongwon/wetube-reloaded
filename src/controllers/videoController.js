9bc50bcc03270eb024c16af2f66d7ff0d7ed3f7c
Yoonseongwon <missingtttt@gmail.com>
2022-01-09 01:48:50 +0900

    FakeDatabaseToBackendAndMixinAndAbsRulvsRelurl

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,19 +1,41 @@
+let videos = [
+    {
+        title: "First Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id:1
 
+    },
+    {
+        title: "Second Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id:2
+
+    },
+    {
+        title: "Third Video",
+        rating: 5,
+        comments: 2,
+        createdAt: "2 minutes ago",
+        views: 59,
+        id:3
+
+    }
+];
 export const trending = (req, res) => {
-    const videos = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
     return res.render("home", {pageTitle : "Home", videos : videos});
 }
-export const see = (req, res) => {
-    console.log(req.params);
-    return res.render("watch");
+export const watch = (req, res) => {
+    const id = req.params.id;
+    const video = videos[id-1];
+    res.render("watch", {pageTitle : `Watching ${video.title}`, video : video});
 }
-export const edit = (req, res) => {
-    console.log(req.params);
-    return res.render("edit");
-}
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("serach");
 export const upload = (req, res) => res.send("Upload Video");
-export const deleteVideo = (req, res) => {
-    console.log(req.params);
-    return res.send("Delete Video");
-}
+export const deleteVideo = (req, res) => res.send("Delete Video");
